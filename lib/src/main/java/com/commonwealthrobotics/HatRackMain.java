b6311d558bb7beb55bbde679513540b9b818f3ea
Kevin Harrington <mad.hephaestus@gmail.com>
2024-06-01 17:11:18 -0400

    initial setup

---
+++ lib/src/main/java/com/commonwealthrobotics/HatRackMain.java
@@ -0,0 +1,12 @@
+package com.commonwealthrobotics;
+
+public class HatRackMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		for(int i=0;i<args.length;i++) {
+			System.out.println("Arg "+i+" = "+args[i]);
+		}
+	}
+
+}
