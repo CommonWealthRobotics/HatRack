06ee3ff52eb9543162d938a37a3ae29791a17b6b
Kevin Harrington <mad.hephaestus@gmail.com>
2024-06-02 14:21:28 -0400

    Working AppImage creation

--- package-linux_x64.sh
+++ package-linux_x64.sh
@@ -1,12 +1,12 @@
 ./gradlew jar
 
 VERSION=0.0.1
-
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 NAME=BowlerStudioUpdater
 ICON=BowlerStudioIcon.png
-rm -rf $NAME
-rm -rf *.AppDir
+rm -rf $SCRIPT_DIR/$NAME
+rm -rf $SCRIPT_DIR/$NAME.AppDir
 
 ~/bin/java17/bin/jpackage --input lib/build/libs/ \
   --name $NAME \
@@ -16,25 +16,42 @@ rm -rf *.AppDir
   --app-version $VERSION \
   --java-options '--enable-preview'
  
-mkdir -p $NAME.AppDir/usr/bin
-cp -r $NAME/* $NAME.AppDir/usr/bin/
+mkdir -p $SCRIPT_DIR/$NAME.AppDir/usr/
+cp -r $SCRIPT_DIR/$NAME/* $SCRIPT_DIR/$NAME.AppDir/usr/
 
 mkdir -p  $NAME.AppDir/usr/share/applications
 mkdir -p  $NAME.AppDir/usr/share/icons/hicolor/256x256/apps
 
 cat << EOF > $NAME.AppDir/usr/share/applications/$NAME.desktop
 [Desktop Entry]
-Name=MyApp
-Exec=$NAME
-Icon=$ICON
+Name=$NAME
+Exec=BowlerStudioUpdater-$ARCH.AppImage
+Icon=$NAME
 Type=Application
-Categories=Utility;
+Categories=Utility
 EOF
- 
-cp $ICON $NAME.AppDir/usr/share/icons/hicolor/256x256/apps/$NAME.png
- 
-# Step 2: Install AppImageTool (done once)
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
 
-./appimagetool-x86_64.AppImage BowlerStudioUpdater.AppDir
+cp $NAME.AppDir/usr/share/applications/$NAME.desktop $NAME.AppDir/$NAME.desktop
+ 
+chmod 644 $SCRIPT_DIR/$NAME.AppDir/usr/share/applications/$NAME.desktop
+
+ln -s usr/bin/$NAME  $SCRIPT_DIR/$NAME.AppDir/AppRun
+ls -l $SCRIPT_DIR/$NAME.AppDir/AppRun
+
+chmod 755 $SCRIPT_DIR/$NAME.AppDir/usr/bin/$NAME
+chmod 755 $SCRIPT_DIR/$NAME.AppDir/AppRun
+
+cp $ICON $NAME.AppDir/usr/share/icons/hicolor/256x256/apps/$NAME.png
+cp $ICON $NAME.AppDir/$NAME.png
+
+# Step 2: Install AppImageTool (done once)
+TOOL=appimagetool-x86_64.AppImage
+if test -f $TOOL; then
+	echo $TOOL exists
+else
+	wget https://github.com/AppImage/AppImageKit/releases/download/continuous/$TOOL
+	chmod +x $TOOL
+fi
+tree $NAME.AppDir
+export ARCH=x86_64
+./$TOOL $NAME.AppDir
