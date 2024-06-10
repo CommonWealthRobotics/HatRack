86882787962a957b9408a1847eea95b1029da666
madhe <madhe@10.0.2.15>
2024-06-09 20:28:01 -0700

    adding the icon file

--- package-windows_x86_64.sh
+++ package-windows_x86_64.sh
@@ -16,19 +16,40 @@ JAR_NAME=lib.jar
 $JAVA_HOME/bin/java.exe -jar $DIR/$JAR_NAME
 echo "Test jar complete"
 NAME=BowlerLauncher
-ICON=splash.ico
+ICON=$NAME.ico
+cp splash.ico $NAME.ico
+
 
 VERSION=0.0.1
 PACKAGE=$JAVA_HOME/bin/jpackage.exe
 mkdir -p "$INPUT_DIR"
 cp "$DIR/$JAR_NAME" "$INPUT_DIR/"
+#rm -rf temp*
+rm -rf $NAME
+$PACKAGE --input "$INPUT_DIR/" --main-jar "$JAR_NAME" --app-version "$VERSION" --icon "$ICON" --type "exe" --resource-dir "temp2" --verbose
+exit 
 # depends on WiX https://github.com/wixtoolset/wix3/releases
 $PACKAGE --input "$INPUT_DIR/" \
   --name "$NAME" \
   --main-jar "$JAR_NAME" \
   --main-class "com.commonwealthrobotics.HatRackMain" \
-  --type "exe" \
-  --temp "temp" --verbose --win-console \
+  --type "app-image" \
+  --temp "temp1" --verbose --win-console \
   --app-version "$VERSION" \
   --icon "$ICON" \
+  --java-options '--enable-preview'
+  
+echo "Zipping standalone version"
+zip -r  $NAME-$VERSION.zip $NAME/
+echo "Building system wide installer" 
+
+$PACKAGE --input "$INPUT_DIR/" \
+  --name "$NAME" \
+  --main-jar "$JAR_NAME" \
+  --main-class "com.commonwealthrobotics.HatRackMain" \
+  --type "exe" \
+  --temp "temp2" --verbose --win-console \
+  --app-version "$VERSION" \
+  --icon "$ICON" \
+  --win-shortcut \
   --java-options '--enable-preview'
